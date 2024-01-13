16c4597fe5b7e4316a08332be1609def33a7ce47
Igor Vremea <igori.vremya@gmail.com>
2024-01-13 19:05:48 +0200

    Ex. IF

---
+++ Lesson1ExIf/Program.cs
@@ -0,0 +1,13 @@
+﻿string name;
+do{
+    Console.Write("Enter your name: ");
+    name = Console.ReadLine();
+
+    if(name.ToLower() == "masha"){
+        Console.WriteLine("HURAY, this is Masha");
+    } else {
+        Console.Write("hi, ");
+        Console.WriteLine(name);
+    }
+} while(name != "ExIt");
+
