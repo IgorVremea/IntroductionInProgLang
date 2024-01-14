03b72075b08edad93da2af4df617ac6eecdcac46
Igor Vremea <igori.vremya@gmail.com>
2024-01-14 16:01:50 +0200

    Функция Аккермана (Рекурсия)

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,26 +1,8 @@
-﻿// Инициализация массива
-int[] arr = new int[10];
-
-Random rand = new Random();
-Console.Write("Было: ");
-for (int i = 0; i < 10; i++){
-    arr[i] = rand.Next(1, 101);
-    Console.Write($"{arr[i]}" + ' ');
-}
-Console.WriteLine();
-
-// Реализация рекурсии
-void ShowRecursArr(int[] arr){
-    if(arr.Length == 1) {
-        Console.Write($"{arr[0]} ");
-        return;
-    }
-    string str = string.Join(' ', arr);
-    // Чойта не допер до более простого решения, если не использовать в функции дополнительный параметр...
-    ShowRecursArr(Array.ConvertAll(str.Substring(str.IndexOf(' ') + 1).Split(' '), int.Parse));
-    Console.Write($"{arr[0]}" + ' ');
+﻿int Ack(int m, int n){
+    if(m == 0) return n+1;
+    if(m > 0 && n == 0) return Ack(m-1, 1);
+    if(m > 0 && n > 0) return Ack(m-1, Ack(m, n-1));
+    return -1;
 }
 
-// Проверка результата
-Console.Write("Стало: ");
-ShowRecursArr(arr);
+Console.WriteLine(Ack(2,2));
