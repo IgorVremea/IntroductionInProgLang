5b0a3858ca2087deb37e43fb68ef4ba4facb6c8b
Igor Vremea <igori.vremya@gmail.com>
2024-01-14 14:24:46 +0200

    Четные числа в массиве

---
+++ Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = new int[10];
+
+int EvenCount(int[] array){
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+Random rand = new Random();
+for (int i = 0; i < 10; i++){
+    arr[i] = rand.Next(1, 101);
+    Console.WriteLine($"{arr[i] }");
+}
+Console.WriteLine(Count2090(arr));
+
