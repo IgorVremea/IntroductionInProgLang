963e5ca673d33fb0aa57092ee836d5bf50e66f1e
Igor Vremea <igori.vremya@gmail.com>
2024-01-14 11:48:37 +0200

    L3 Functions

--- Lesson3Functions/Program.cs
+++ Lesson3Functions/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Method1(int count, string str){
+    for(int i = 0; i < count; i++){
+        Console.WriteLine(str);
+    }
+}
+
+Method1(2, "String");
+Console.WriteLine();
+Method1(str: "Stroka", count: 5);
+
