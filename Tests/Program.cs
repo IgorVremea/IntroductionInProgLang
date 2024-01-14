09999e1b75c073bc63e63e46f0d1dc52e7b33d8f
Igor Vremea <igori.vremya@gmail.com>
2024-01-14 14:25:14 +0200

    Числа от 20 до 90 в массиве

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,17 +1,16 @@
 ﻿int[] arr = new int[10];
 
-int EvenCount(int[] array){
+int Count2090(int[] array){
     int count = 0;
     for(int i = 0; i < array.Length; i++){
-        if(array[i] % 2 == 0) count++;
+        if(array[i] >= 20 && array[i] <= 90) count++;
     }
     return count;
 }
-
 Random rand = new Random();
 for (int i = 0; i < 10; i++){
-    arr[i] = rand.Next(1, 101);
+    array[i] = rand.Next(1, 101);
     Console.WriteLine($"{arr[i] }");
 }
-Console.WriteLine(Count2090(arr));
+Console.WriteLine(Count2090(*arr));
 
