6abbece3b2302edc3de8c252004f05d971d4c64c
Igor Vremea <igori.vremya@gmail.com>
2024-01-14 12:22:27 +0200

    L4 Recursion

---
+++ Lesson4Recurs/Program.cs
@@ -0,0 +1,9 @@
+﻿int Factorial(int n){
+    if(n==0) return 1;
+        else return n * Factorial(n-1);
+}
+
+Console.WriteLine(Factorial(2));
+Console.WriteLine(Factorial(3));
+Console.WriteLine(Factorial(4));
+Console.WriteLine(Factorial(5));
