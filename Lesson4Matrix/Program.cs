12a62de53447797e6141f525e87fe21d7bbba06b
Igor Vremea <igori.vremya@gmail.com>
2024-01-14 12:14:13 +0200

    L4 Matrix

---
+++ Lesson4Matrix/Program.cs
@@ -0,0 +1,9 @@
+﻿int[,] matrix = new int[3,4];
+int count = 1;
+for(int y = 0; y < matrix.GetLength(0); y++){
+    for(int x = 0; x < matrix.GetLength(1); x++){
+        matrix[y,x] = count++;
+        Console.Write($"{matrix[y,x]} ");
+    }
+    Console.WriteLine();
+}
